b891f7b10a25ebf7fe427c7eab7ec0ce332a708e
azunaVT <36605286+azunaVT@users.noreply.github.com>
2025-08-09 06:31:03 +0000

    Adjusted IPs for cilium install

--- infra/modules/cilium/main.tf
+++ infra/modules/cilium/main.tf
@@ -9,20 +9,22 @@ data "helm_template" "cilium" {
   create_namespace = true
   namespace        = "kube-system"
 
-  set = [
-    { name = "ipam.mode", value = "kubernetes" },
-    { name = "kubeProxyReplacement", value = "true" },
-    { name = "securityContext.capabilities.ciliumAgent", value = "{CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}" },
-    { name = "securityContext.capabilities.cleanCiliumState", value = "{NET_ADMIN,SYS_ADMIN,SYS_RESOURCE}" },
-    { name = "cgroup.autoMount.enabled", value = "false" },
-    { name = "cgroup.hostRoot", value = "/sys/fs/cgroup" },
-    { name = "k8sServiceHost", value = "localhost" },
-    { name = "k8sServicePort", value = "7445" },
-    { name = "gatewayAPI.enabled", value = "true" },
-    { name = "gatewayAPI.enableAlpn", value = "true" },
-    { name = "gatewayAPI.enableAppProtocol", value = "true" },
-    { name = "cni.exclusive", value = "false" }
-  ]
+  # set = [
+  #   { name = "ipam.mode", value = "kubernetes" },
+  #   { name = "kubeProxyReplacement", value = "true" },
+  #   { name = "securityContext.capabilities.ciliumAgent", value = "{CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}" },
+  #   { name = "securityContext.capabilities.cleanCiliumState", value = "{NET_ADMIN,SYS_ADMIN,SYS_RESOURCE}" },
+  #   { name = "cgroup.autoMount.enabled", value = "false" },
+  #   { name = "cgroup.hostRoot", value = "/sys/fs/cgroup" },
+  #   { name = "k8sServiceHost", value = "localhost" },
+  #   { name = "k8sServicePort", value = "7445" },
+  #   { name = "gatewayAPI.enabled", value = "true" },
+  #   { name = "gatewayAPI.enableAlpn", value = "true" },
+  #   { name = "gatewayAPI.enableAppProtocol", value = "true" },
+  #   { name = "cni.exclusive", value = "false" }
+  # ]
+
+  values = [ file("${path.module}/values.yaml") ]
 }
 
 # Outputs
